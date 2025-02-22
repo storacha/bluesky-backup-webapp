10655095959006fb8ecaaa64df4434a22ec4d873
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-22 10:31:30 +0700

    feat: add login and callback handlers

--- src/app/api/bluesky-auth/callback/route.ts
+++ src/app/api/bluesky-auth/callback/route.ts
@@ -1,5 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ message: 'Callback' });
+  const searchParams = request.nextUrl.searchParams;
+  const session = searchParams.get('session');
+  const state = searchParams.get('state');
+
+  if (!session || !state) {
+    return NextResponse.json({ message: 'Bad request' }, { status: 400 });
+  }
+
+  return NextResponse.json({ message: 'Authenticated' });
 }
--- src/app/api/bluesky-auth/login/route.ts
+++ src/app/api/bluesky-auth/login/route.ts
@@ -1,5 +1,23 @@
+import { blueskyClient } from "@/instances/bluesky";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json({ message: 'Login' });
+  const searchParams = request.nextUrl.searchParams;
+  const handle = searchParams.get('handle');
+  const state = searchParams.get('state');
+
+  if (!handle || !state) {
+    return NextResponse.json({ message: 'Bad request' }, { status: 400 });
+  }
+
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const url = await blueskyClient.authorize(handle, {
+    signal: signal,
+    state,
+    ui_locales: 'fr-CA fr en',
+  });
+
+  return NextResponse.redirect(url);
 }
