08251a159e52066c7bfdee80fd880bd8d5f80ceb
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-02-22 10:24:10 +0700

    feat: add redis and handlers for set auth state and session

---
+++ src/services/blueskyAuth.ts
@@ -0,0 +1,34 @@
+import { redis } from "@/instances/redis";
+import { NodeSavedSession, NodeSavedState } from "@atproto/oauth-client-node";
+
+export const setStateStore = async (key: string, internalState: NodeSavedState): Promise<void> => {
+  await redis.set(key, JSON.stringify(internalState));
+}
+
+export const getStateStore = async (key: string): Promise<NodeSavedState | undefined> => {
+  const state = await redis.get(key);
+  if (!state) {
+    return;
+  }
+  return JSON.parse(state);
+}
+
+export const delStateStore = async (key: string): Promise<void> => {
+  await redis.del(key);
+}
+
+export const setSessionStore = async (sub: string, session: NodeSavedSession): Promise<void> => {
+  await redis.set(sub, JSON.stringify(session));
+}
+
+export const getSessionStore = async (sub: string): Promise<NodeSavedSession | undefined> => {
+  const session = await redis.get(sub);
+  if (!session) {
+    return;
+  }
+  return JSON.parse(session);
+}
+
+export const delSessionStore = async (sub: string): Promise<void> => {
+  await redis.del(sub);
+}
