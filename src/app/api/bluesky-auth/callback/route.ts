9d76b5e9a747480644dfe718bf5f5b4716c11d07
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2025-03-02 18:23:55 +0700

    Add callback based on example

--- src/app/api/bluesky-auth/callback/route.ts
+++ src/app/api/bluesky-auth/callback/route.ts
@@ -1,13 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { blueskyServerClient } from "@/lib/blueskyServerAuth"
+import { Agent } from "@atproto/api"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const session = searchParams.get('session');
-  const state = searchParams.get('state');
+  const searchParams = request.nextUrl.searchParams
 
-  if (!session || !state) {
-    return NextResponse.json({ message: 'Bad request' }, { status: 400 });
+  const { session, state } = await blueskyServerClient.callback(searchParams)
+  const token = await session.getTokenInfo()
+  
+  console.log('authorize() was called with state:', state)
+  console.log('User authenticated as:', session.did)
+  console.log('Token:', token)
+
+  const agent = new Agent(session)
+  if (!agent.did) {
+    return NextResponse.json({ message: 'Bad request' }, { status: 400 })
   }
 
-  return NextResponse.json({ message: 'Authenticated' });
+  const profile = await agent.getProfile({ actor: agent.did })
+  console.log('Bsky profile:', profile.data)
+
+  return NextResponse.json({ ok: true })
 }
