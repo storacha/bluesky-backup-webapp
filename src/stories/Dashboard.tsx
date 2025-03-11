acb256d34a0eb338831270f6829dc5e61d8de706
Travis Vachon <travis.vachon@gmail.com>
2025-03-11 00:20:29 -0700

    feat: add DashboardPage to Storybook
    This shows off the whole page layout

--- src/stories/Dashboard.tsx
+++ src/stories/Dashboard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react';
 
 import './button.css';
@@ -17,8 +19,8 @@ export const Dashboard = () => {
   const { backupsStore } = useBackupsContext()
   const backups = useLiveQuery(() => backupsStore.listBackups())
   return (
-    <div className="flex flex-col space-y-4">
-      <div>
+    <div className="flex flex-col space-y-4 items-start mt-16">
+      <div className="bg-white/80 backdrop-blur-3xl p-16 rounded border border-bluesky-blue">
         {bluesky.initialized ? (
           bluesky.authenticated ? (
             storacha.accounts?.[0] ? (
@@ -27,14 +29,20 @@ export const Dashboard = () => {
               <StorachaAuthenticator />
             )
           ) : (
-            <BlueskyAuthenticator />
+            <div>
+              <h3 className="text-2xl font-bold mb-16">Welcome to <span className="text-storacha-red">HOT</span> Bluesky Backups by Storacha!</h3>
+              <div className="flex flex-col items-center">
+                <h4 className="mb-4 font-bold">To get started, please log in to your Bluesky account:</h4>
+                <BlueskyAuthenticator />
+              </div>
+            </div>
           )
         ) : (
           <Loader />
         )}
       </div>
-      <div>
-        <h2 className="text-sm font-mono font-bold uppercase mb-2">Backups</h2>
+      <div className="w-full bg-white/80 backdrop-blur-3xl p-16 rounded border border-bluesky-blue">
+        <h2 className="font-mono font-bold uppercase mb-2">Backups</h2>
         {backups ? (backups.length > 0 ? (
           <Backups />
         ) : (
